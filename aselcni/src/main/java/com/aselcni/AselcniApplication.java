886fe3ca28ad7805c0d52ee39ce0e2dc45251663
dlgkstnrn <51310682+dlgkstnrn@users.noreply.github.com>
2024-03-25 17:40:34 +0900

    이한수 최초커밋

---
+++ aselcni/src/main/java/com/aselcni/AselcniApplication.java
@@ -0,0 +1,13 @@
+package com.aselcni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AselcniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AselcniApplication.class, args);
+	}
+
+}
