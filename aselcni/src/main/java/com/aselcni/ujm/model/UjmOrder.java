5b87190fbd79c697ac2812ac7ecdc7cfd0e8a5c0
jaman <ujm1jaman@gmail.com>
2024-04-01 15:11:41 +0900

    뷰테스트

--- aselcni/src/main/java/com/aselcni/ujm/model/UjmOrder.java
+++ aselcni/src/main/java/com/aselcni/ujm/model/UjmOrder.java
@@ -13,4 +13,5 @@ public class UjmOrder { //주문
 	private String order_update; //주문수정일
 	private int    order_delete_chk; //주문삭제여부
 	private int    order_status_chk; //주문상태
+	private String order_end_dt; //주문마감일
 }
