23caac9d4179005cdaf97a3af9a23f5be7b838ef
Eueukk <rgnmd1235@gmail.com>
2024-04-02 10:04:34 +0900

    240402 KDW 컨피그 커어밋

--- aselcni/src/main/java/com/aselcni/kdw/controller/KdwProdPlanController.java
+++ aselcni/src/main/java/com/aselcni/kdw/controller/KdwProdPlanController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.aselcni.kdw.model.TB_ITEM_PROD;
 import com.aselcni.kdw.model.TB_PRODPLAN;
 import com.aselcni.kdw.service.KdwProdPlanService;
 
@@ -22,8 +23,13 @@ public class KdwProdPlanController {
 	// 계획생산 리스트 조회(달력폼) 이동
 	@GetMapping(value = "prodplan")
 	public String getProdPlanMainForm(Model model) {
-		System.out.println("KdwProductionPlanningController getproductionPlanningForm Start...");
+		System.out.println("KdwProductionPlanningController getProdPlanMainForm Start...");
+		// 제품 리스트
 		List<TB_PRODPLAN> prodPlans = kdwProdPlanService.getProdPlanList();
+		System.out.println("KdwProductionPlanningController getProdPlanMainForm prodPlans.size(): " + prodPlans.size());
+		// 자재 리스트(제품하나에 여러 자재)
+		List<TB_ITEM_PROD> prodItems = kdwProdPlanService.getProdItemList();
+		System.out.println("KdwProductionPlanningController getProdPlanMainForm prodItems.size(): " + prodItems.size());
 		return "kdw/kdwProductionPlanning";
 	}
 }
