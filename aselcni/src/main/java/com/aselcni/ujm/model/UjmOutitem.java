1e1c5e1dc0da99a1dc9f576de467fa528403ed85
jaman <ujm1jaman@gmail.com>
2024-04-03 14:35:03 +0900

    마스터통합전

--- aselcni/src/main/java/com/aselcni/ujm/model/UjmOutitem.java
+++ aselcni/src/main/java/com/aselcni/ujm/model/UjmOutitem.java
@@ -10,7 +10,6 @@ public class UjmOutitem { //출고
 	private String outitem_dt; //출고일자
 	private String cust_cd; //거래처CD(고객사), fk
 	private String cust_emp; //담당자명(거래처)
-	private String item_cd; //품목CD, fk
 	private int    outitem_qty; //출고수량
 	private String remark; //비고
 	private String outitem_emp_id; //출고담당자ID
