34c43857f8a346c36288c5b010829074d6c2d3f5
chobo112 <wjdtod12ahdl@naver.com>
2024-04-04 14:24:24 +0900

    master통합완료

--- aselcni/src/main/java/com/aselcni/ujm/model/UjmOutitem.java
+++ aselcni/src/main/java/com/aselcni/ujm/model/UjmOutitem.java
@@ -7,17 +7,6 @@ public class UjmOutitem { //출고
 	private String outitem_no; //출고번호, PK
 	private int    seq_no; //순번
 	private String order_no; //주문번호, fk
-<<<<<<< HEAD
-	private String outitem_dt; //주문일자
-	private String cust_cd; //거래처CD(고객사), fk
-	private String cust_emp; //담당자명(거래처)
-	private String item_cd; //품목CD, fk
-	private int    outitem_qty; //출고수량
-	private String remark; //비고
-	private String outitem_emp_id; //출고담당자ID
-	private String outitem_update; //출고수정일
-	private int    outitem_delete_chk; //출고삭제여부
-=======
 	private String outitem_dt; //출고일자
 	private String cust_cd; //거래처CD(고객사), fk
 	private String cust_emp; //담당자명(거래처)
@@ -45,5 +34,4 @@ public class UjmOutitem { //출고
 	private String item_nm;
 	private int    qty;
 	private String user_nm;
->>>>>>> branch 'master' of https://github.com/dlgkstnrn/aselCNI.git
 }
