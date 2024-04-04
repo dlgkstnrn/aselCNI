56d2f08e168c510c07f22b527483c11fc8f63551
park-jinh <jinh.drive@gmail.com>
2024-04-04 14:53:33 +0900

    [박진형] - search데이터 삭제

--- aselcni/src/main/java/com/aselcni/pjh/model/PJHInitem.java
+++ aselcni/src/main/java/com/aselcni/pjh/model/PJHInitem.java
@@ -25,8 +25,8 @@ public class PJHInitem {
 	private String item_nm;
 	private String start_date;
 	private String end_date;
-	private String searchItemNm;
-	private String searchCustNm;
+	//private String searchItemNm;
+	//private String searchCustNm;
 	
 	// page 정보
 	private int startPage; // 시작 페이지
